544161d756afb458b9f616bc587ca14f5a86967e
shwetap44 <31558213+shwetap44@users.noreply.github.com>
2023-04-04 22:47:27 +0530

    added gdrive dapp

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+
+
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  paths: {
+    artifacts: "./client/src/artifacts",
+  },
+};
